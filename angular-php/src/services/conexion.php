e5c8cd04e5399a8deedc905948f8e85cfa039a4a
Engel <erestrepo@cbt.es>
2023-03-09 11:49:20 +0100

    .

--- angular-php/src/services/conexion.php
+++ angular-php/src/services/conexion.php
@@ -5,34 +5,34 @@
 //   return $con;
 // }
 
-// function retornarConexion() {
-//     $servidor = "pruebaspipex.esy.es";
-//     $usuario = "u381394134_engel";
-//     $contraseña = "Lancelot23$";
-//     $base_datos = "u381394134_angular";
+function retornarConexion() {
+    $servidor = "pruebaspipex.esy.es";
+    $usuario = "u381394134_engel";
+    $contraseña = "Lancelot23$";
+    $base_datos = "u381394134_angular";
   
-//     $con = mysqli_connect($servidor, $usuario, $contraseña, $base_datos);
+    $con = mysqli_connect($servidor, $usuario, $contraseña, $base_datos);
     
-//     // Verifica si hay algún error en la conexión
-//     if (mysqli_connect_errno()) {
-//       echo "Error al conectar a la base de datos: " . mysqli_connect_error();
-//       exit();
-//     }  
-//     return $con;
-// }
+    // Verifica si hay algún error en la conexión
+    if (mysqli_connect_errno()) {
+      echo "Error al conectar a la base de datos: " . mysqli_connect_error();
+      exit();
+    }  
+    return $con;
+}
 
-  $servername = "localhost";
-  $username = "u381394134_engel";
-  $password = "Lancelot23$";
-  $database = "u381394134_angular";
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $database);
-  // Check connection
-  if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-  }
-  echo "Connected successfully";
-  mysqli_close($conn);  
+//   $servername = "localhost";
+//   $username = "u381394134_engel";
+//   $password = "Lancelot23$";
+//   $database = "u381394134_angular";
+//   // Create connection
+//   $conn = mysqli_connect($servername, $username, $password, $database);
+//   // Check connection
+//   if (!$conn) {
+//       die("Connection failed: " . mysqli_connect_error());
+//   }
+//   echo "Connected successfully";
+//   mysqli_close($conn);  
 
 ?>
 
